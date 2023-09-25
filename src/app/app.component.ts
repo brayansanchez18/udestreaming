7725078a6628ace925e011a8f9f8e39a5ed0e6dc
Brayan Emanuel Sanchez Ramirez <brayan.sanchez.contacto@gmail.com>
2023-09-24 20:05:44 -0600

    creacion del frontend y uso de la plantill html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+declare var $: any;
+declare function HOME_INIT([]): any;
+declare function INIT_SWIPER([]): any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'streaming_tipo-nexflit';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    setTimeout(() => {
+      HOME_INIT($);
+      INIT_SWIPER($);
+    }, 50);
+  }
 }
